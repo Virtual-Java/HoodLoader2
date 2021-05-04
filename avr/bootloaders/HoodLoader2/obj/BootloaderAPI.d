e73c9c650bf552312bba8c3b43b5afec4f8784f3
Jonathan Vetter <Virtual.Java@t-online.de>
2021-05-04 14:57:51 +0200

    output gcc10 compile error

--- avr/bootloaders/HoodLoader2/obj/BootloaderAPI.d
+++ avr/bootloaders/HoodLoader2/obj/BootloaderAPI.d
@@ -6,23 +6,13 @@ obj/BootloaderAPI.o: BootloaderAPI.c BootloaderAPI.h \
  ../lufa/LUFA/../LUFA/Common/CompilerSpecific.h \
  ../lufa/LUFA/../LUFA/Common/Attributes.h Config/LUFAConfig.h \
  ../lufa/LUFA/../LUFA/Common/Endianness.h Config/AppConfig.h
-
 BootloaderAPI.h:
-
 ../lufa/LUFA/../LUFA/Common/Common.h:
-
 ../lufa/LUFA/../LUFA/Common/Architectures.h:
-
 ../lufa/LUFA/../LUFA/Common/BoardTypes.h:
-
 ../lufa/LUFA/../LUFA/Common/ArchitectureSpecific.h:
-
 ../lufa/LUFA/../LUFA/Common/CompilerSpecific.h:
-
 ../lufa/LUFA/../LUFA/Common/Attributes.h:
-
 Config/LUFAConfig.h:
-
 ../lufa/LUFA/../LUFA/Common/Endianness.h:
-
 Config/AppConfig.h:
