c7f259bb8c423da0559668f82ff6071d474c741e
Jonathan Vetter <Virtual.Java@t-online.de>
2021-07-25 20:54:52 +0200

    Improve SoftwareReset and PowerSwitch to consume only 4 Bytes each

--- avr/bootloaders/HoodLoader2/Board/Board.h
+++ avr/bootloaders/HoodLoader2/Board/Board.h
@@ -27,19 +27,29 @@ extern "C" {
 #endif
 
 
-/**  Arduino modification to support DebugWire
-*  Capacitor between DTR (pin 20) of USB MCU and RESET pin (PB6) of main MCU.
-*  Besides it enables ISP programming without capacitor
-*  between main MCU's RESET and GND */
+/** • SoftwareReset:		(Arduino modification to support DebugWire)
+*  Due to SoftwareReset it is possible to optionaly replace the capacitor
+*  between DTR (pin 20) of the USB MCU and RESET pin (PB6) of the main MCU by a resistor.
+*  Besides it enables ISP programming without a bypass capacitor of 10 μF
+*  usually necessary to be connected to main MCU's RESET and GND.
+*  This modification consumes 4 Bytes of flash memory only
+*  and the best thing is it doesn't affect upload to Arduinos with capacitor!
+*/
 #define USING_SOFTWARE_RESET
 
-/**  Arduino modification to support DebugWire
+/** • PowerSwitch:			(Arduino modification to support DebugWire)
 *  Mosfet soldered between +5V and the output of the voltage regulator
 *  to switch on/off power supply of the main MCU by software running on USB MCU.
 *  However the power supply of the USB MCU is not affected.
 *  The default VCCEN pin is PB5 to control the active low p-channel mosfet.
+*  This modification consumes 4 Bytes of flash memory additionally
+*  and doesn't affect standard Arduinos at all!
 */
 #define WITH_VCC_ENABLE
+#ifdef WITH_VCC_ENABLE
+	#define VCCEN_ACTIVE_HIGH  false // false (default) for p-ch Mosfet (recommanded)
+	// change this to true for n-ch Mosfet (tricky to use because of inflated GS-Voltage)
+#endif
 
 	/* Includes: */
 		#include <LUFA/Common/Common.h>
@@ -232,10 +242,14 @@ extern "C" {
 		#if !defined(__DOXYGEN__)
 			static inline void Board_Init(void)
 			{
+				/* switch on the main MCU */
 				#ifdef WITH_VCC_ENABLE
-					AVR_VCCEN_LINE_DDR  |= ~AVR_VCCEN_LINE_MASK;	// declare VCCEN pin (def.PB5) as output
-					AVR_VCCEN_LINE_PORT &= ~AVR_VCCEN_LINE_MASK;	// VCCEN is LOW active (p-ch mosfet)
-					//AVR_VCCEN_LINE_PORT |= AVR_VCCEN_LINE_MASK;	// VCCEN is HIGH active (n-ch mosfet)
+					AVR_VCCEN_LINE_DDR  |= AVR_VCCEN_LINE_MASK; // Set VCCEN pin to output
+					#if(VCCEN_ACTIVE_HIGH)
+						AVR_VCCEN_LINE_PORT |= AVR_VCCEN_LINE_MASK; // VCCEN is HIGH active (n-ch mosfet)
+					#else
+						AVR_VCCEN_LINE_PORT &= ~AVR_VCCEN_LINE_MASK; // VCCEN is LOW active (p-ch mosfet)
+					#endif
 				#endif
 				DDRD |= LEDS_ALL_LEDS | (1 << PD3) | AVR_RESET_LINE_MASK;
 				PORTD |= AVR_RESET_LINE_MASK;
@@ -253,7 +267,13 @@ extern "C" {
 				{
 					AVR_RESET_LINE_PORT &= ~AVR_RESET_LINE_MASK;
 					#ifdef USING_SOFTWARE_RESET
-					_delay_us(100);  // wait 100 microsecords to reset main MCU on boards with resistor instead of capacitor on reset line
+					// Changing this delay to specific values between 3 and 42 μs saves some bytes of flash (due to perfect timer interrupt)
+					// 4 Bytes for 3, 6, 12, 18, 24, 30, 36, 42 μs
+					// 6 Bytes for 1, 2, 5, 7, 8, 10, 20, 40, 46, μs,
+					// 10 Bytes for 200, 500, 1000 μs (values > 48 μs)
+					// 1, 2, 3, 5, 12, 20, 40, 200, 500 μs values tested with Arduino Uno R3 @ 16 MHz, so the valid range is: 1 to 500 μs; values like 800, 1000 μs didn't work for me
+					// 12 μs seams to bee a good default value
+					_delay_us(12);  // wait 12 microsecords to reset main MCU on boards with resistor instead of capacitor on reset line
 					AVR_RESET_LINE_PORT |= AVR_RESET_LINE_MASK;
 					#endif
 				}
